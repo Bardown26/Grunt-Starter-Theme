1e97956068a66c8fc4bbe37759fd5124075a7ccf
Andrew Atieh <andrewatieh@Andrews-MBP.home>
2015-06-18 13:59:45 -0400

    adding tags to the bottom of the tag.php post page and single page

--- single.php
+++ single.php
@@ -18,6 +18,7 @@
                         Written by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a> | <?php the_time('F j, Y');?> | <?php the_category( ', ');?>
                     </div>
                     <?php the_content();?>
+                    <?php the_tags();?>
                 </article>
             <?php endwhile; ?>
 
--- tag.php
+++ tag.php
@@ -1,19 +1,38 @@
 <?php get_header(); ?>
+<div class="container">
+    <?php if (function_exists('the_breadcrumbs')) the_breadcrumbs(); ?>
+    <div class="section">
+        <div class="main-content col span8-12">
+            <h1>Tag Archive: <?php echo single_tag_title('', false); ?></h1>
+            <?php if ( have_posts() ) : ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+                <?php while( have_posts() ) : the_post(); ?>
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <a href="<?php the_permalink();?>"><h2><?php the_title();?></h2></a>
+                        <div class="meta">
+                            Written by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a> | <?php the_time('F j, Y');?> | <?php the_category( ', ');?>
+                        </div>
+                        <?php the_excerpt();?>
+                        <p class="more"><a class="orange-btn" href="<?php get_permalink($post->ID);?>">Read More</a></p>
 
-			<?php get_template_part('loop'); ?>
+                    </article>
+                <?php endwhile; ?>
 
-			<?php get_template_part('pagination'); ?>
 
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+                <nav id="nav-below">
+                    <?php if (function_exists("pagination")) {
+                        pagination();
+                    } ?>
+                </nav><!-- #nav-below -->
+            <?php else : ?>
+                <?php get_template_part( 'content', 'none' ); ?>
+            <?php endif; ?>
+        </div> <!-- end main-content -->
+        <div class="sidebar col span4-12">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
